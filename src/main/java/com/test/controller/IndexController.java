0f68bbbde7f791ed54f17d96476ca9c324707775
wanyy15083 <1277503624@qq.com>
2017-06-13 21:45:57 +0800

    增加页面文件

--- src/main/java/com/test/controller/IndexController.java
+++ src/main/java/com/test/controller/IndexController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
-public class ManageController {
+@RequestMapping
+public class IndexController {
 
     @RequestMapping(value = "/index")
     public ModelAndView index() {
--- src/test/java/com/test/zookeeper/CuratorDemo.java
+++ src/test/java/com/test/zookeeper/CuratorDemo.java
@@ -5,15 +5,29 @@ import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.api.BackgroundCallback;
 import org.apache.curator.framework.api.CuratorEvent;
+import org.apache.curator.framework.recipes.atomic.AtomicValue;
+import org.apache.curator.framework.recipes.atomic.DistributedAtomicInteger;
+import org.apache.curator.framework.recipes.barriers.DistributedBarrier;
+import org.apache.curator.framework.recipes.barriers.DistributedDoubleBarrier;
 import org.apache.curator.framework.recipes.cache.NodeCache;
 import org.apache.curator.framework.recipes.cache.NodeCacheListener;
 import org.apache.curator.framework.recipes.cache.PathChildrenCache;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheListener;
+import org.apache.curator.framework.recipes.leader.LeaderSelector;
+import org.apache.curator.framework.recipes.leader.LeaderSelectorListenerAdapter;
+import org.apache.curator.framework.recipes.locks.InterProcessLock;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.retry.RetryNTimes;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.data.Stat;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.test.context.TestPropertySource;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -23,6 +37,8 @@ import java.util.concurrent.Executors;
  */
 public class CuratorDemo {
     private CuratorFramework client;
+    private static DistributedBarrier barrier;
+
 
     private static CountDownLatch latch = new CountDownLatch(2);
     private static ExecutorService es = Executors.newFixedThreadPool(2);
@@ -31,6 +47,8 @@ public class CuratorDemo {
     public static final String ROOT = "/test";
     public static final String DATA = "123";
     public static final String CHILD = "/123";
+    public static final String MASTER = "/master";
+    public static final String LOCK = "/lock";
 
     @Before
     public void init() {
@@ -123,8 +141,133 @@ public class CuratorDemo {
 
     @Test
     public void testPathChildrenCache() throws Exception {
-        PathChildrenCache cache = new PathChildrenCache(client,ROOT+CHILD,true);
+        PathChildrenCache cache = new PathChildrenCache(client, ROOT, true);
         cache.start(PathChildrenCache.StartMode.POST_INITIALIZED_EVENT);
+        cache.getListenable().addListener(new PathChildrenCacheListener() {
+            @Override
+            public void childEvent(CuratorFramework client, PathChildrenCacheEvent event) throws Exception {
+                switch (event.getType()) {
+                    case CHILD_ADDED:
+                        System.out.println("CHILD_ADDED," + event.getData());
+                        break;
+                    case CHILD_UPDATED:
+                        System.out.println("CHILD_UPDATED," + event.getData());
+                        break;
+                    case CHILD_REMOVED:
+                        System.out.println("CHILD_REMOVED," + event.getData());
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
 
+        client.create().withMode(CreateMode.PERSISTENT).forPath(ROOT);
+        Thread.sleep(1000);
+        client.create().withMode(CreateMode.PERSISTENT).forPath(ROOT + CHILD);
+        Thread.sleep(1000);
+        client.delete().forPath(ROOT + CHILD);
+        Thread.sleep(1000);
+        client.delete().forPath(ROOT);
+        Thread.sleep(Integer.MAX_VALUE);
     }
+
+    @Test
+    public void testMasterSelect() throws InterruptedException {
+        LeaderSelector selector = new LeaderSelector(client, MASTER, new LeaderSelectorListenerAdapter() {
+            @Override
+            public void takeLeadership(CuratorFramework client) throws Exception {
+                System.out.println("be master");
+                Thread.sleep(3000);
+                System.out.println("complete master,release");
+            }
+        });
+        selector.autoRequeue();
+        selector.start();
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+
+    @Test
+    public void testLock() throws InterruptedException {
+        final InterProcessMutex lock = new InterProcessMutex(client, LOCK);
+        final CountDownLatch down = new CountDownLatch(1);
+        for (int i = 0; i < 30; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        lock.acquire();
+                        down.await();
+                    } catch (Exception e) {
+                    }
+                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss|sss");
+                    String orderNo = sdf.format(new Date());
+                    System.out.println("generate orderNo:" + orderNo);
+                    try {
+                        lock.release();
+                    } catch (Exception e) {
+                    }
+                }
+            }).start();
+        }
+        down.countDown();
+
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+
+    @Test
+    public void testDistAtomic() throws Exception {
+        DistributedAtomicInteger atomicInteger = new DistributedAtomicInteger(client, LOCK, new RetryNTimes(3, 1000));
+        AtomicValue<Integer> atomicValue = atomicInteger.add(8);
+        System.out.println(atomicValue.succeeded());
+    }
+
+    @Test
+    public void testBrrier() throws Exception {
+        for (int i = 0; i < 5; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    barrier = new DistributedBarrier(client, "/barrier");
+                    System.out.println(Thread.currentThread().getName() + "号barrier设置");
+                    try {
+                        barrier.setBarrier();
+                        barrier.waitOnBarrier();
+                        System.out.println("启动。。。");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }).start();
+        }
+        Thread.sleep(2000);
+        barrier.removeBarrier();
+    }
+
+    @Test
+    public void testBarrier2() throws InterruptedException {
+        for (int i = 0; i < 5; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        DistributedDoubleBarrier barrier = new DistributedDoubleBarrier(client, "/barrier", 5);
+                        Thread.sleep(Math.round(Math.random() * 3000));
+                        System.out.println(Thread.currentThread().getName() + "号进入barrier");
+                        barrier.enter();
+                        System.out.println("启动...");
+                        Thread.sleep(Math.round(Math.random() * 3000));
+                        barrier.leave();
+                        System.out.println("退出...");
+                    } catch (Exception e) {
+                    }
+
+                }
+            }).start();
+        }
+
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+
 }
